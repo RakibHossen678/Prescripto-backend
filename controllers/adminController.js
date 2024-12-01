4770688a3b9311b95e954225c72b076126534456
RakibHossen678 <rh491464@gmail.com>
2024-12-01 21:25:43 +0600

    Add: user endpoints

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,8 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import { v2 as cloudinary } from "cloudinary";
+import doctorModel from "../models/doctorModel.js";
+
 // Controller function for adding a doctor
 const addDoctor = async (req, res) => {
   try {
@@ -14,22 +19,71 @@ const addDoctor = async (req, res) => {
     } = req.body;
     const imageFile = req.file;
 
-    console.log({
+    //checking for all data to doctor
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !speciality ||
+      !degree ||
+      !experience ||
+      !about ||
+      !fees ||
+      !address
+    ) {
+      return res.status(400).json({ message: "Please fill all the fields" });
+    }
+
+    //validating email format
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+    //validating password
+    if (validator.isLength < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 8 characters" });
+    }
+
+    //hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Checking if an image file is provided
+    if (!imageFile) {
+      return res.status(400).json({ message: "Please upload a doctor image" });
+    }
+
+    // Uploading image to Cloudinary
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const imageUrl = imageUpload.secure_url;
+
+    const doctorData = {
       name,
       email,
-      password,
+      image: imageUrl,
+      password: hashedPassword,
       speciality,
       degree,
       experience,
       about,
       fees,
-      address,
-      imageFile,
-    });
+      address: JSON.parse(address),
+      date: Date.now(),
+    };
+    const newDoctor = new doctorModel(doctorData);
+    await newDoctor.save();
+    res.status(201).json({ message: "Doctor created successfully" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Something went wrong", error });
   }
 };
+
+
+//api for the admin login 
+
 
 export { addDoctor };
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -4,7 +4,6 @@ import upload from "../middlewares/multer.js";
 
 const adminRouter = express.Router();
 
-
 adminRouter.post("/add-doctor", upload.single("image"), addDoctor);
 
 export default adminRouter;
